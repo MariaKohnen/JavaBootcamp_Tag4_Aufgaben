7a83d069dcacd7db6b6bffc864d8789596554310
Maria Kohnen <maria.kohnen@gmail.com>
2022-03-24 16:04:10 +0100

    Initial

--- src/main/java/Novice.java
+++ src/main/java/Novice.java
@@ -1,2 +1,26 @@
 public class Novice {
+
+    public static void main(String[] args) {
+        System.out.println("Sönke Wehnert & Maria Kohnen");
+
+        for (int i = 1; i <= 5; i++ ) {
+            System.out.println("Sönke Wehnert & Maria Kohnen");
+        }
+    }
+
+    public static boolean isGreaterThanZero(int number) {
+        return number > 0;
+
+        /* Alternative
+
+        if (number <= 0) {
+            return false;
+        } else {
+            return true;
+        }
+         */
+    }
+
+
 }
+
---
+++ src/test/java/NoviceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoviceTest {
+
+    @Test
+    void isGreaterThanZero_whenNumber0_shouldFalse() {
+        assertFalse(Novice.isGreaterThanZero(0));
+    }
+
+    @Test
+    void isGreaterThanZero_whenNumberMinus_shouldFalse() {
+        assertFalse(Novice.isGreaterThanZero(-1));
+    }
+
+    @Test
+    void isGreaterThanZero_whenNumberTwo_shouldTrue() {
+        assertTrue(Novice.isGreaterThanZero(2));
+    }
+
+}
